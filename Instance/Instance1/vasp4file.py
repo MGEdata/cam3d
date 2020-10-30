ee81f544f3fc8f189277d1f3273b23a243e46433
boliqq07 <986798607@qq.com>
2020-10-30 16:38:34 +0800

    first

---
+++ Instance/Instance1/vasp4file.py
@@ -0,0 +1,52 @@
+from pymatgen.io.vasp.inputs import Incar,Poscar,Kpoints,VaspInput,PotcarSingle,Potcar
+from pymatgen.io.cif import CifParser,CifBlock,CifWriter
+from pymatgen import MPRester
+from pymatgen import Composition, Element
+import itertools
+import IPython
+import random
+API_KEY = 'Di2IZMunaeR8vr9w' ##Materialsproject自己号带的
+name = "mp-3626"
+def que_p(target): #分子式，比如Fe2O3
+    a = MPRester(API_KEY)
+    data=a.get_data(target,data_type='vasp',prop="cif")
+    return data
+data = que_p("mp-3626")
+data=data[0]["cif"]
+
+cif = CifParser.from_string(data)
+structure = cif.get_structures(data)[0]
+############
+# cifwriter = CifWriter(structure)
+# cifwriter.write_file("cif")
+############
+
+pos = Poscar(structure)
+
+kpo = Kpoints.automatic_density_by_vol(structure, 300, force_gamma=False)
+
+incat_str = """
+ALGO = Fast
+EDIFF = 0.002
+ENCUT = 520
+IBRION = 2
+ISIF = 3
+ISMEAR = -5
+ISPIN = 2
+LASPH = True
+LORBIT = 11
+LREAL = Auto
+LWAVE = False
+MAGMOM = 40*0.6
+NELM = 100
+NSW = 99
+PREC = Accurate
+SIGMA = 0.05
+"""
+
+incar = Incar.from_string(incat_str)
+symbols = pos.site_symbols
+pot = Potcar(symbols=["Sr_sv","Zr_sv","O"])
+
+file4 = VaspInput(incar, kpo, pos, pot, optional_files={"cif":cif})
+file4.write_input(r"%s"%name)
---
+++ Instance/Instance1/vaspoutfile.py
@@ -0,0 +1,25 @@
+
+from pymatgen.io.vasp.outputs import Chgcar,Elfcar,Vasprun,Outcar,Xdatcar
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from pymatgen.io.vasp.outputs import Vasprun
+from pymatgen.electronic_structure.plotter import BSDOSPlotter
+
+####dos#####
+# # read vasprun.xml，get band and dos information
+# bs_vasprun = Vasprun("vasprun.xml", parse_projected_eigen=True)
+# bs_data = bs_vasprun.get_band_structure(line_mode=True)
+#
+# dos_vasprun = Vasprun("vasprun.xml")
+# dos_data = dos_vasprun.complete_dos
+
+# # set figure parameters, draw figure
+# banddos_fig = BSDOSPlotter(bs_projection='elements', dos_projection='elements',
+#                            )
+# banddos_fig.get_plot(bs=bs_data, dos=dos_data)
+
+#########
+# outcar =Outcar(r'mp-ele-inoc/OUTCAR')
+chgcar =Chgcar.from_file(r'mp-3626-refine/CHGCAR')
+# xdatcar = Xdatcar(r'mp-3626-refine/XDATCAR')
+
